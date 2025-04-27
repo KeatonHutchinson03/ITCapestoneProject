7a6438ee456228dfebd104a4ae5d7001afd56a4a
Virata24 <ajvirata15@gmail.com>
2025-04-27 11:14:18 -0400

    new working movement

--- movement.js
+++ movement.js
@@ -8,237 +8,246 @@ let isCrouching = false;
 
 let parryButtonPressed = false;
 let parryActive = false;
-let parryTimeOut;
+let parryTimeout;
 
-let mainAttackButtonPressed = false;
 let mainAttacking = false;
 let mainAttackHitBox;
-let mainAttackDamage = 10; // random value to decide how much health we want the main attack to do
+let mainAttackDamage = 10;
 
-let specialAttackButtonPressed = false;
 let specialAttacking = false;
-let specialAttackHitBox = false;
-let specialAttackDamage = 30; // random value we can change as well
-let canSpcialAttack = true;
-let specialAttackCoolDown = 30000 // 30 sec cooldown on special attack
+let specialAttackHitBox;
+let specialAttackDamage = 30;
+let canSpecialAttack = true;
+let specialAttackCooldown = 30000;
 
 let isBlocking = false;
-let blockReduction = 0.5; // will still take damage while blocking but will only take reducded damage.
+let blockReduction = 0.5;
 
-let secondaryAttackButtonPressed = false;
 let secondaryAttacking = false;
 let secondaryAttackHitBox;
-let secodaryAttackDamage = 5;
+let secondaryAttackDamage = 5;
 
-function preload() {
-// Load the player image or sprite
-this.load.image('player', 'path/to/player/mouse.png');
-this.load.image('enemy', 'path/to/enemy/cat.png');
-}
+const config = {
+    type: Phaser.AUTO,
+    width: 800,
+    height: 600,
+    backgroundColor: '#222',
+    physics: {
+        default: 'arcade',
+        arcade: { gravity: { y: 500 }, debug: true }
+    },
+    scene: {
+        preload,
+        create,
+        update
+    }
+};
+
+const game = new Phaser.Game(config);
+
+function preload() {}
 
 function create() {
-// Set up the player sprite with physics
-player = this.physics.add.sprite(100, 450, 'player'); // where the player is at believe we can get rid of the 100 and 450
-player.setBounce(0.2);
-player.setCollideWorldBounds(true);
-player.setGravityY(300); // Gravity that pulls the player down
-player.health = 100;
+    // Generate block textures for player and enemy
+    this.textures.generate('player', {
+        data: ['.11.', '1111', '.11.'],
+        pixelWidth: 20
+    });
 
-enemy = this.physics.add.sprite('enemy');// creates the enemy // supposed to be enemy = this.physics.add.sprite(400, 450, 'enemy'); loads the enemy at this point but idk
-enemy.health = 30; // could be the cat or something but need to grab the enery we want with their id
+    this.textures.generate('enemy', {
+        data: ['2222', '2..2', '2222'],
+        pixelWidth: 20
+    });
 
-this.physics.add.sprite(mainAttackHitBox,enemy,hitEnemy, null ,this); // allows the enemy to get hit by main attack
+    // Set up player and enemy sprites
+    player = this.physics.add.sprite(100, 450, 'player');
+    player.setCollideWorldBounds(true);
+    player.setBounce(0.2);
+    player.health = 100;
 
-// You can set up gamepad controls here
-this.input.gamepad.once('connected', function (pad) {
-console.log('Gamepad connected: ', pad);
-});
+    enemy = this.physics.add.sprite(400, 450, 'enemy');
+    enemy.setCollideWorldBounds(true);
+    enemy.health = 50;
+
+    // Create hitboxes for attacks
+    mainAttackHitBox = this.add.rectangle(0, 0, 40, 40, 0xff0000).setVisible(false);
+    this.physics.add.existing(mainAttackHitBox);
+    this.physics.add.overlap(mainAttackHitBox, enemy, () => hitEnemy(enemy, mainAttackDamage), null, this);
+
+    specialAttackHitBox = this.add.rectangle(0, 0, 60, 60, 0x00ffff).setVisible(false);
+    this.physics.add.existing(specialAttackHitBox);
+    this.physics.add.overlap(specialAttackHitBox, enemy, () => hitEnemy(enemy, specialAttackDamage), null, this);
+
+    secondaryAttackHitBox = this.add.rectangle(0, 0, 30, 30, 0xffff00).setVisible(false);
+    this.physics.add.existing(secondaryAttackHitBox);
+    this.physics.add.overlap(secondaryAttackHitBox, enemy, () => hitEnemy(enemy, secondaryAttackDamage), null, this);
 }
 
 function update() {
-// Check for gamepad input every frame
+    // Ensure the gamepad system is ready and a gamepad is connected
+   
+    const pads = navigator.getGamepads();
+    if (pads[0]) {
+        gamepad = pads[0]; // Get the first gamepad
 
-// Get the first connected gamepad (0 index)
-gamepad = this.input.gamepad.gamepads[0];
+        // Check if buttons are pressed
+        if (gamepad.buttons[0].pressed && !jumpButtonPressed) { // A button (jump)
+            jumpButtonPressed = true;
+            if (player.body.touching.down) {  // Check if the player is on the ground
+                player.setVelocityY(-330);  // Apply an upward velocity (jump)
+                console.log("Jump!");
+            }
+        } else if (!gamepad.buttons[0].pressed) { // Reset jump when button is released
+            jumpButtonPressed = false;
+        }
 
-// If the gamepad is connected and has the A button pressed, make the player jump
-if (gamepad && gamepad.buttons[0].pressed && !jumpButtonPressed) { // 0 is typically the A button on Xbox controllers
-jumpButtonPressed = true;
-if (player.body.touching.down) { // Check if the player is on the ground
-player.setVelocityY(-330); // Apply an upward velocity (jump)
-}
-}
-// If the A button is released, allow for subsequent jumps
-if (!gamepad.buttons[0].pressed) { // A jump button
-jumpButtonPressed = false;
-}
+        // Handle movement based on the left stick (X-axis)
+        if (gamepad.axes[0] < -0.2) {
+            player.setVelocityX(-160);
+        } else if (gamepad.axes[0] > 0.2) {
+            player.setVelocityX(160);
+        } else {
+            player.setVelocityX(0);
+        }
 
+        // Handle crouch (B button)
+        if (gamepad.buttons[1].pressed && !crouchButtonPressed) {
+            crouchButtonPressed = true;
+            crouch();
+        } else if (!gamepad.buttons[1].pressed && crouchButtonPressed) {
+            crouchButtonPressed = false;
+            uncrouch();
+        }
 
-if (gamepad.axes[0] < -0.1) { //
-player.setVelocityX(-160); // Move left
-} else if (gamepad.axes[0] > 0.1) {
-player.setVelocityX(160); // Move right
-} else {
-player.setVelocityX(0); // No movement on the x-axis
-}
+        // Handle parry (LB button)
+        if (gamepad.buttons[4].pressed && !parryButtonPressed) {
+            parryButtonPressed = true;
+            activateParry();
+        } else if (!gamepad.buttons[4].pressed) {
+            parryButtonPressed = false;
+        }
 
-if(!gamepad.buttons[1].pressed){ //1 = B button on xbox controller
-crouchButtonPressed = true;
-crouch();
-}
-if (!gamepad.buttons[1].pressed) {
-crouchButtonPressed = false;
-if (isCrouching) {
-uncrouch();
-}
-}
+        // Handle main attack (RT button)
+        if (gamepad.buttons[7].pressed) {
+            mainAttack();
+        }
 
-if(!gamepad.buttons[4].pressed && !parryButtonPressed){ //4 = LB
-parryButtonPressed = true;
-activateParry();
-}
-if(!gamepad.buttons[4].pressed){
-parryButtonPressed = false;
-}
+        // Handle secondary attack (LT button)
+        if (gamepad.buttons[6].pressed) {
+            secondaryAttack();
+        }
 
-if(!gamepad.buttons[7].pressed){ //7 = RT
-mainAttackButtonPressed = true;
-mainAttack();
-}
-if(!gamepad.buttons[7].pressed){ // RT
-mainAttackButtonPressed = false;
-}
-if(!gamepad.buttons[6].pressed){ //LT
-secondaryAttackButtonPressed = true;
-secondaryAttack();
-}
-if(!gamepad.buttons[6].pressed){ //LT
-secondaryAttackButtonPressed = false;
-}
-if(!gamepad.buttons[5].pressed && canSpcialAttack){ //RB
-specialAttackButtonPressed = true;
-performSpecialAttack();
-}
-if(!gamepad.buttons[5].pressed){ //RB
-specialAttackButtonPressed = false;
+        // Handle special attack (RB button)
+        if (gamepad.buttons[5].pressed && canSpecialAttack) {
+            performSpecialAttack();
+        }
 
-}
-if(!gamepad.buttons[2].pressed){ //X
-startBlocking();
-} else{
-stopBlocking();
-}
+        // Handle blocking (X button)
+        if (gamepad.buttons[2].pressed) {
+            startBlocking();
+        } else {
+            stopBlocking();
+        }
+    } else {
+        console.log("No gamepad detected");
+    }
 }
 
-function crouch(){
-if(!isCrouching){
-player.setScale(1,0.5);
-console.log("Crouching")
-player.setGravityY(150); // optional if we want to make it so they have crouch jump they have less gravity
-isCrouching = true;
 
-player.body.setSize(player.width, player.height *0.5); // changes collusiuin size
-}
-}
-
-function uncrouch(){
-if(!isCrouching){
-player.setScale(1,1);
-player.setGravityY(300);
-console.log("Uncrouch") // test to see if we reading input
-isCrouching = false;
-player.body.setSize(player.width, player.height); //sets back to normal
-}
+function crouch() {
+    if (!isCrouching) {
+        console.log("Crouching");
+        player.setScale(1, 0.5);
+        player.setGravityY(200);
+        player.body.setSize(player.width, player.height * 0.5);
+        isCrouching = true;
+    }
 }
 
-function activateParry(){
-if(!parryActive){
-console.log("Parry Attempted") //just an output to see if the parry button is even working
-parryActive = true;
-
-parryTimeOut = setInterval(() => {
-console.log("Parry window closed") // agaian just to see if the parrying window is opening and closing
-parryActive = false;
-}, 200 ); // 200 ms parry window time we can change this to make it harder or easier based on how we feel
-}
+function uncrouch() {
+    if (isCrouching) {
+        console.log("Uncrouch");
+        player.setScale(1, 1);
+        player.setGravityY(500);
+        player.body.setSize(player.width, player.height);
+        isCrouching = false;
+    }
 }
 
-function mainAttack(){ // main attack feature i believe can be changed if we have different weapons in our hands but we can try that later or if we just want the main attack to be the same and change our special attack
-if(!mainAttacking){
-console.log("Main Attack") //testing to see if we getting the input
-
-mainAttacking = true;
-mainAttackHitBox.setPosition(player.x + 40 , player.y + 10); // hitbox of the attack can change this if we dont like the height or wideth of hitbox
-mainAttackHitBox.setVisible(true); // shows the hitbox
-
-setTimeout(() => {
-mainAttackHitBox.setVisible(false);
-mainAttacking = false;
-}, 200);
-}
+function activateParry() {
+    if (!parryActive) {
+        console.log("Parry Activated");
+        parryActive = true;
+        parryTimeout = setTimeout(() => {
+            console.log("Parry window closed");
+            parryActive = false;
+        }, 200);
+    }
 }
 
-function secondaryAttack(){ // if this doesnt work main attack doesnt work legit same thing just different values
-if(!secondaryAttacking){
-console.log("Secondary Attack")
-
-secondaryAttacking = true;
-secondaryAttackHitBox.setPosition(player.x + 20, player.x +5);
-secondaryAttackHitBox.setVisible(true);
-
-setTimeout(() => {
-secondaryAttackHitBox.setVisible(false);
-secondaryAttacking = false;
-})
-}
+function mainAttack() {
+    if (!mainAttacking) {
+        console.log("Main Attack!");
+        mainAttacking = true;
+        mainAttackHitBox.setPosition(player.x + 40, player.y);
+        mainAttackHitBox.setVisible(true);
+        setTimeout(() => {
+            mainAttackHitBox.setVisible(false);
+            mainAttacking = false;
+        }, 200);
+    }
 }
 
-function hitEnemy(mainAttackHitBox,specialAttackHitBox,secondaryAttackHitBox,enemy){ // that shows we are hitting the enemy we can get rid of the console logs once we know each function works believe we have to pass through everytime we make a new attack
-console.log("Enemy was hit -" + mainAttackDamage + "HP");
-
-enemy.health -= mainAttackDamage;
-
-if(enemy.health <0 ){
-console.log("Enemy was slain!")
-enemy.destroy();
-} else{
-console.log("Enemy Health " + enemy.health);
-}
+function secondaryAttack() {
+    if (!secondaryAttacking) {
+        console.log("Secondary Attack");
+        secondaryAttacking = true;
+        secondaryAttackHitBox.setPosition(player.x + 20, player.y);
+        secondaryAttackHitBox.setVisible(true);
+        setTimeout(() => {
+            secondaryAttackHitBox.setVisible(false);
+            secondaryAttacking = false;
+        }, 200);
+    }
 }
 
 function performSpecialAttack() {
-console.log(" Special Attack Activated! ");
-
-enemy.health -= specialAttackDamage;
-specialAttackHitBox.setPosition(player.x + 40, player.y +10); //size of speicla attack we cna change
-specialAttackHitBox.setVisible(true); //shows if the speicla attack is being seenhitbox
-
-console.log(`Enemy hit! Took ${specialDamage} damage. Health left: ${enemy.health}`);
-
-if (enemy.health <= 0) {
-console.log("Enemy Defeated!");
-enemy.destroy();
+    console.log("Special Attack Activated!");
+    specialAttackHitBox.setPosition(player.x + 40, player.y);
+    specialAttackHitBox.setVisible(true);
+    setTimeout(() => {
+        specialAttackHitBox.setVisible(false);
+    }, 200);
+    canSpecialAttack = false;
+    setTimeout(() => {
+        canSpecialAttack = true;
+        console.log("Special Attack Ready!");
+    }, specialAttackCooldown);
 }
 
-// Start Cooldown
-canSpecialAttack = false;
-setTimeout(() => {
-canSpecialAttack = true;
-console.log(" Special Attack Ready!");
-}, specialAttackCoolDown);
+function hitEnemy(enemy, damage) {
+    console.log(`Enemy hit for ${damage} damage`);
+    enemy.health -= damage;
+    if (enemy.health <= 0) {
+        console.log("Enemy defeated!");
+        enemy.destroy();
+    } else {
+        console.log("Enemy HP:", enemy.health);
+    }
 }
 
-function startBlocking(){
-if(!isBlocking){
-console.log("Attacked Blocked")
-isBlocking = true;
-}
+function startBlocking() {
+    if (!isBlocking) {
+        console.log("Started blocking");
+        isBlocking = true;
+    }
 }
 
-function stopBlocking(){
-if(isBlocking){
-console.log("Stopped blocking")
-isBlocking = false;
+function stopBlocking() {
+    if (isBlocking) {
+        console.log("Stopped blocking");
+        isBlocking = false;
+    }
 }
-}
+
   
